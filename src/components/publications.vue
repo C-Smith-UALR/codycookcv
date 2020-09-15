735aa805ec6a4738b828cd87d6c355851e446f8d
medzoarDev <medzoar@gmail.com>
2020-09-15 13:13:25 -0500

    completed the publications accordion

--- src/components/publications.vue
+++ src/components/publications.vue
@@ -21,7 +21,7 @@
                     <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
                         <b-card-body>
                             <p>
-                                C.C. Cook, A. Kim, S. Terao, A.Gotoh, and M. Higuchi.  Consumption
+                                C.C. Cook, A. Kim, S. Terao, A. Gotoh, and M. Higuchi.  Consumption
                                 of oxygen: a mitochondrial-generated progression signal of advanced cancer.
                                 Cell Death and Disease, 3, e258.
                             </p>
@@ -39,10 +39,67 @@
                     </b-card-header>
                     <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
                         <b-card-body>
-                            <b-card-text>{{ text }}</b-card-text>
+                            <b-card-text>
+                                A. Naito, C. Cook, T. Mizumachi, M. Wang, C. Xie, T. Evans, T. Kelley,
+                                and M. Higuchi.  Progressive tumor features accompany epithelial-mesenchymal
+                                transition induced in mitochondrial DNA depleted cells.  Cancer Science, 99(8): 1584
+                            </b-card-text>
                         </b-card-body>
                     </b-collapse>
                 </b-card>
+
+                <b-card no-body class="mb-1">
+                    <b-card-header header-tag="header" class="p-1" role="tab">
+                        <b-button block v-b-toggle.accordion-4 variant="dark">2006</b-button>
+                    </b-card-header>
+                    <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
+                        <b-card-body>
+                            <b-card-text>
+                                D.R. Pierce, C.C. Cook, J.A. Hinson, and K.E. Light.  Are oxidative mechanisms
+                                primary in ethanol induced purkinje neuron death of the neonatal rat?  Neuroscience
+                                Letters, 400(1-2), 130
+                            </b-card-text>
+                        </b-card-body>
+                    </b-collapse>
+                </b-card>
+
+                <b-card no-body class="mb-1">
+                    <b-card-header header-tag="header" class="p-1" role="tab">
+                        <b-button block v-b-toggle.accordion-5 variant="dark">2005</b-button>
+                    </b-card-header>
+                    <b-collapse id="accordion-5" accordion="my-accordion" role="tabpanel">
+                        <b-card-body>
+                            <b-card-text>
+                                Cook, C., Haley, U.B. and Runge, S.W.  Acidification Induced Exocytic Response in
+                                MCF-7 Cells and Correlating Apoptosis.  Arkansas BRIN Research Conference. Little Rock,
+                                AR.
+                            </b-card-text>
+                            <b-card-text>
+                                UCA Honors College Dissertation.  Title: The evolution of the art of fly-tying: How scientific,
+                                social, and industrial revolutions of the Victorian age influenced the art of fly-tying.
+                            </b-card-text>
+                        </b-card-body>
+                    </b-collapse>
+                </b-card>
+                <b-card no-body class="mb-1">
+                    <b-card-header header-tag="header" class="p-1" role="tab">
+                        <b-button block v-b-toggle.accordion-6 variant="dark">2004</b-button>
+                    </b-card-header>
+                    <b-collapse id="accordion-6" accordion="my-accordion" role="tabpanel">
+                        <b-card-body>
+                            <b-card-text>
+                                D.R. Pierce, C.C. Cook, J.A. Hinson, and K.E. Light.  Are oxidative mechanisms primary in ethanol induced purkinje
+                                neuron death of the neonatal rate?  Alcoholism: Clinical and Experimental Research, 28(86A)
+                            </b-card-text>
+                            <b-card-text>
+                                H.R. Owens, V. Chen, C.C. Cook, D.R. Pierce, and K.E. Light.  Comparative ability of NMDA receptor
+                                antagonists and ethanol in producing neurodegeneration of rat purkinje neurons during
+                                postnatal vulnerable periods.  Alcoholism: Clinical and Experiemental Research, 28(86A)
+                            </b-card-text>
+                        </b-card-body>
+                    </b-collapse>
+                </b-card>
+
             </div>
         </div>
     </section>
